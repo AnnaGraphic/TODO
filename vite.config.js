1cd11993f2bf6951378e0c0dffb326e3f8a2262d
panda <p4nd4@posteo.de>
2023-12-11 16:39:39 +0100

    setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // set base to repo for deploy on gh-pages
+  base: '/TODO/',
   plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
 })
